987e1b033d7e7362edc45d27ee6f044f950cd8d5
Cayden Lunt <cayden.lunt@gmail.com>
2026-03-20 02:16:23 -0600

    completed week03 learning

--- week03/learn/Program.cs
+++ week03/learn/Program.cs
@@ -9,5 +9,29 @@ public class Program
 
         Console.WriteLine("\n======================\nTranslator\n======================");
         Translator.Run();
+
+        // Write code to find the first time in a string when a letter is duplicated.
+        // Answer each of the following:
+        //    - What are possible scenarios to consider? (For example, think of a few strings that you would want to try with your solution.)
+        //        - "racecar" palindrome
+        //        - "and" should return -9999, nan, etc. based on preference or requirements
+        //
+        //    - What are some data structures that may be useful? And what would their performance be?
+        //        - you could use a hash table or set at O(n) complexity
+        //        - you could use an array or list which could also be done at O(n) complexity
+        //        - you could use a pre-populated dictionary<string, bool> populated with keys a-z and all values at false
+        //
+        //    - What are the boundary conditions that you should consider for this problem?
+        //        - strings of differing lengths
+        //        - one or both strings being empty
+        //        - strings with special characters (many can be stored as text but should they be omitted or do they count)
+        //        - handling quotation
+        //        - really long strings
+        //        - really short strings 
+        //        - numbers (they can be stored as text but should they be omitted or do they count)
+        //
+        //    - Outline a possible solution.
+        //        - My recommendation would be to use a list. there is a maximum of 26 possible letters meaning a maximum size of 26.
+        //          dictionaries and hash maps are overkill. Don't over engineer. 
     }
 }
