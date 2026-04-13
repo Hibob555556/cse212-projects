b74be7f9f5ade6a0e3dd243c1f844364d8da610f
Cayden Lunt <cayden.lunt@gmail.com>
2026-04-13 17:39:46 -0600

    Implement BST contains, reverse traversal, height, and balanced insert

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Node
 {
     public int Data { get; set; }
@@ -21,7 +23,7 @@ public class Node
             else
                 Left.Insert(value);
         }
-        else
+        else if (value > Data)
         {
             // Insert to the right
             if (Right is null)
@@ -29,17 +31,50 @@ public class Node
             else
                 Right.Insert(value);
         }
+        else
+        {
+            return;
+        }
     }
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value < Data)
+        {
+            // Look to the left
+            if (Left is null)
+                return false;
+            else
+                return Left.Contains(value);
+        }
+        else if (value > Data)
+        {
+            // Look to the right 
+            if (Right is null)
+                return false;
+            else
+                return Right.Contains(value);
+        }
+        else
+        {
+            // the value is equal to data, return true
+            return true;
+        }
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = 0;
+        int rightHeight = 0;
+
+        if (Left is not null)
+            leftHeight = Left.GetHeight();
+
+        if (Right is not null)
+            rightHeight = Right.GetHeight();
+
+        if (leftHeight > rightHeight)
+            return 1 + leftHeight;
+        return 1 + rightHeight;
     }
 }
